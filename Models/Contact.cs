ca7996d0051b49207c3da102d962fb75638d7079
Panupong Onthuen <luis206639-st@rmutsb.ac.th>
2019-09-16 20:29:24 +0700

    first commit

---
+++ Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Email { get; set;  }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+        public bool Sendcopy { get; set; }
+
+
+
+    }
+    }
